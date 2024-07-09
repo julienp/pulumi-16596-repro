80fd2dbfa210a6db5422d37bdc2192f00739d73b
Julien Poissonnier <julien@caffeine.lu>
2024-07-09 13:09:40 +0200

    heap stats

--- infra/index.ts
+++ infra/index.ts
@@ -1,23 +1,33 @@
 import * as os from "node:os"
 import * as fs from "node:fs"
-import * as az from "@pulumi/azure-native";
+import * as v8 from "node:v8"
 import * as pulumi from "@pulumi/pulumi";
+
+console.log(v8.getHeapStatistics())
+
+const az = require("@pulumi/azure-native");
+
+console.log(v8.getHeapStatistics())
 
 function mb(n: number): number {
   return Math.round(n / 1024 / 1024);
 }
 
-const out = fs.createWriteStream("out.txt", { flags: "a" })
 
+const out = fs.createWriteStream("out.txt", { flags: "a" })
 out.write(`RSS\t\tTotal\tFree\n`)
+out.end()
 console.log(`RSS\t\tTotal\tFree`)
 
 
 let i = 0
 let int = setInterval(() => {
   const mem = process.memoryUsage();
+  const out = fs.createWriteStream("out.txt", { flags: "a" })
   out.write(`${mb(mem.rss)}\t${mb(os.totalmem())}\t${mb(os.freemem())}\n`)
+  out.end()
   console.log(`${mb(mem.rss)}\t${mb(os.totalmem())}\t${mb(os.freemem())}`)
+  console.log(v8.getHeapStatistics())
   i++
   if (i > 5) {
     clearInterval(int)
